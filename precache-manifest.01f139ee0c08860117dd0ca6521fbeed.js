a132e0192745fcfc3037323c89cf0c2858a09488
Abhinn Ankit <abhinnankit@outlook.com>
2019-05-02 04:24:51 -0400

    Updates

---
+++ precache-manifest.01f139ee0c08860117dd0ca6521fbeed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9f9a3db6f8d44fc409486ee2c386d9",
+    "url": "abhinnankit.github.io/index.html"
+  },
+  {
+    "revision": "5e58409073b85266dd94",
+    "url": "abhinnankit.github.io/static/css/main.8a6f7dcf.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "abhinnankit.github.io/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "5e58409073b85266dd94",
+    "url": "abhinnankit.github.io/static/js/main.6de32c5e.chunk.js"
+  },
+  {
+    "revision": "9bf7cdc42c764bd68001",
+    "url": "abhinnankit.github.io/static/js/runtime~main.1c8345e1.js"
+  }
+]);
--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './App.scss';
-
-const App: React.FC = () => {
-    return <div className="App">blah</div>;
-};
-
-export default App;
