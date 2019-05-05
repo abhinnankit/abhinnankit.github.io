45c4b44467120160f99a97877cb2cefca4ee7eba
Abhinn Ankit <abhinnankit@outlook.com>
2019-05-05 05:04:53 -0400

    Reduced image size

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import AboutMe from './AboutMe/AboutMe';
+import classes from './About.module.scss';
 
 const about = props => {
-    const sectionClasses = ['col-md-6', 'col-12'];
+    const sectionClasses = ['col-md-6', 'col-12', classes.About];
     return (
-        <section className={sectionClasses.join(' ')} style={{ display: 'flex' }}>
+        <section className={sectionClasses.join(' ')}>
             <AboutMe />
         </section>
     );
