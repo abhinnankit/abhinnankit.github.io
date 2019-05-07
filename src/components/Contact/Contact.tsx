ecfc79e036bcd4c9208b7099c15756f8753929ff
Abhinn Ankit <abhinnankit@outlook.com>
2019-05-07 01:15:47 -0400

    Update

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,33 +5,25 @@ const contact = props => {
     const textareaClasses = [classes.textarea, classes.ContactFields];
     const inputFieldClasses = [classes.ContactFields];
     return (
-        <div className={classes.Contact}>
-            <form action="https://formspree.io/abhinnankit@outlook.com" method="POST" className={classes.Form}>
-                <input
-                    className={inputFieldClasses.join(' ')}
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                    required={true}
-                />
-                <input
-                    className={inputFieldClasses.join(' ')}
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    required={true}
-                />
-                <textarea
-                    className={textareaClasses.join(' ')}
-                    name="message"
-                    placeholder="Your message"
-                    spellCheck={false}
-                />
-                <button className={classes.btn} type="submit">
-                    Submit
-                </button>
-            </form>
-        </div>
+        <form action="https://formspree.io/abhinnankit@outlook.com" method="POST" className={classes.Contact}>
+            <input className={inputFieldClasses.join(' ')} type="text" name="name" placeholder="Name" required={true} />
+            <input
+                className={inputFieldClasses.join(' ')}
+                type="email"
+                name="email"
+                placeholder="Email"
+                required={true}
+            />
+            <textarea
+                className={textareaClasses.join(' ')}
+                name="message"
+                placeholder="Your message"
+                spellCheck={false}
+            />
+            <button className={classes.btn} type="submit">
+                Submit
+            </button>
+        </form>
     );
 };
 
