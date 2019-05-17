30269f567a435ea2e7c36ee6b5e1725c4356bd25
Abhinn Ankit <abhinnankit@outlook.com>
2019-05-16 22:39:10 -0400

    Add header animation

--- src/hoc/Layout/Layout.tsx
+++ src/hoc/Layout/Layout.tsx
@@ -10,6 +10,12 @@ import Home from '../../components/Home/Home';
 class Layout extends Component {
     state = {
         markActive: [false, false, false, false],
+        animateHeader: [false, false, false],
+    };
+
+    height = {
+        work: 0,
+        about: 0,
     };
 
     private readonly aboutRef: React.RefObject<any>;
@@ -23,6 +29,11 @@ class Layout extends Component {
         this.workRef = React.createRef<HTMLElement>();
         this.contactRef = React.createRef<HTMLElement>();
         this.headerRef = React.createRef<Header>();
+    }
+
+    isDesktop() {
+        const isMobile = window.matchMedia('only screen and (min-width: 768px)');
+        return isMobile.matches;
     }
 
     linkClicked = navItem => {
@@ -45,7 +56,7 @@ class Layout extends Component {
     addClasses = () => {
         const headerHeight = this.headerRef.current.height + this.headerRef.current.headerRef.current.offsetHeight - 53;
         const aboutHeight = this.aboutRef.current.offsetHeight + this.headerRef.current.height - 55;
-        const workHeight = aboutHeight + this.workRef.current.offsetHeight;
+        const workHeight = aboutHeight + this.workRef.current.offsetHeight + 80;
         if (window.pageYOffset >= workHeight || window.innerHeight + window.scrollY >= document.body.offsetHeight) {
             if (!this.state.markActive[3]) {
                 this.setState({
@@ -71,11 +82,47 @@ class Layout extends Component {
                 });
             }
         }
+        if (this.isDesktop()) {
+            this.addAnimateClass();
+        }
+    };
+
+    addAnimateClass = () => {
+        const headerHeight =
+            this.headerRef.current.height + this.headerRef.current.headerRef.current.offsetHeight - 330;
+        const aboutHeight = this.height.about + this.headerRef.current.height - 330;
+        const workHeight = aboutHeight + this.height.work;
+        if (window.pageYOffset >= workHeight || window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+            if (!this.state.animateHeader[2]) {
+                const animateHeader = this.state.animateHeader.slice();
+                animateHeader[2] = true;
+                this.setState({
+                    animateHeader,
+                });
+            }
+        } else if (window.pageYOffset >= aboutHeight) {
+            if (!this.state.animateHeader[1]) {
+                const animateHeader = this.state.animateHeader.slice();
+                animateHeader[1] = true;
+                this.setState({
+                    animateHeader,
+                });
+            }
+        } else if (window.pageYOffset >= headerHeight) {
+            const animateHeader = this.state.animateHeader.slice();
+            animateHeader[0] = true;
+            if (!this.state.animateHeader[0]) {
+                this.setState({
+                    animateHeader,
+                });
+            }
+        }
     };
 
     componentDidMount(): void {
+        this.height.about = this.aboutRef.current.clientHeight + 156;
+        this.height.work = this.workRef.current.clientHeight + 460;
         window.addEventListener('scroll', this.addClasses);
-        this.addClasses();
     }
 
     componentWillUnmount(): void {
@@ -83,14 +130,13 @@ class Layout extends Component {
     }
 
     render() {
-        if (window.matchMedia('only screen and (min-width: 1440px)')) {
-        }
         return (
             <>
                 <Home />
                 <Header ref={this.headerRef} clicked={this.linkClicked} markActive={this.state.markActive} />
                 <main>
                     <Section
+                        animateH1ZoomIn={this.state.animateHeader[0]}
                         ref={this.aboutRef}
                         title={'ABOUT'}
                         style={{ backgroundColor: '#f0f0f0', paddingTop: '10em' }}
@@ -100,7 +146,12 @@ class Layout extends Component {
                     <div
                         style={{ background: 'linear-gradient(to right top, #e5e0da 50%, #f0f0f0 50%)', height: '5em' }}
                     />
-                    <Section ref={this.workRef} title={'Projects'} style={{ backgroundColor: '#e5e0da' }}>
+                    <Section
+                        animateH1ZoomIn={this.state.animateHeader[1]}
+                        ref={this.workRef}
+                        title={'Projects'}
+                        style={{ backgroundColor: '#e5e0da' }}
+                    >
                         <Projects />
                     </Section>
                     <div
@@ -109,7 +160,12 @@ class Layout extends Component {
                             height: '5em',
                         }}
                     />
-                    <Section ref={this.contactRef} title="contact" style={{ backgroundColor: '#f0f0f0' }}>
+                    <Section
+                        animateH1ZoomIn={this.state.animateHeader[2]}
+                        ref={this.contactRef}
+                        title="contact"
+                        style={{ backgroundColor: '#f0f0f0' }}
+                    >
                         <Contact />
                     </Section>
                 </main>
--- src/hoc/Section/Section.tsx
+++ src/hoc/Section/Section.tsx
@@ -5,17 +5,24 @@ import Row from '../Row/Row';
 
 interface IProps {
     title: string;
+    animateH1ZoomIn?: boolean;
     style?: object;
 }
 
 const section: RefForwardingComponent<HTMLElement, IProps & { ref: Ref<HTMLElement> }> = React.forwardRef(
     (props, ref) => {
+        const h1Classes = [classes.h1, classes.opaque];
+        const sectionUnderlineClass = [classes.SectionUnderline, classes.opaque];
+        if (props.animateH1ZoomIn) {
+            h1Classes.push(classes.animateZoomIn);
+            sectionUnderlineClass.push(classes.animateZoomIn);
+        }
         return (
             <section ref={ref} className={classes.Section} style={props.style}>
                 <Container>
                     <Row style={{ flexDirection: 'column' }}>
-                        <h1>{props.title.toUpperCase()}</h1>
-                        <div className={classes.SectionUnderline} />
+                        <h1 className={h1Classes.join(' ')}>{props.title.toUpperCase()}</h1>
+                        <div className={sectionUnderlineClass.join(' ')} />
                     </Row>
                     <Row>{props.children}</Row>
                 </Container>
