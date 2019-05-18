2352d1191f1a650f7ee8cbc573a003ba18bea18f
Abhinn Ankit <abhinnankit@outlook.com>
2019-05-17 22:55:38 -0400

    Add banner image

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import classes from './Home.module.scss';
+import banner from '../../assets/images/banner.jpg';
 
 const home = () => {
-    return <div className={classes.Home} />;
+    return <div className={classes.Home} style={{ backgroundImage: `url(${banner})` }} />;
 };
 
 export default home;
