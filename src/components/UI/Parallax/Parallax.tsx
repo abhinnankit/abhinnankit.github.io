2d044452f1ef6beb80a5bbd85181d45474c9fd4c
Abhinn Ankit <abhinnankit@outlook.com>
2019-05-19 17:21:08 -0400

    Add parallax mobile support

--- src/components/UI/Parallax/Parallax.tsx
+++ src/components/UI/Parallax/Parallax.tsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import classes from './Parallax.module.scss';
 import imgUrl from '../../../assets/images/banner.png';
 
 const Parallax = () => {
+    const wrapperRef = useRef(null);
+    const parallaxMobileRef = useRef(null);
+    const [bottom, setBottom] = useState(0);
+    const onScroll = () => {
+        console.dir(
+            window.innerHeight -
+                wrapperRef.current.getBoundingClientRect().top +
+                ' ' +
+                (window.innerHeight + wrapperRef.current.getBoundingClientRect().height)
+        );
+        // window.requestAnimationFrame(scrollImage);
+        if (
+            window.innerHeight - wrapperRef.current.getBoundingClientRect().top >
+            window.innerHeight + wrapperRef.current.getBoundingClientRect().height
+        ) {
+            parallaxMobileRef.current.style.position = 'absolute';
+            parallaxMobileRef.current.style.bottom = bottom;
+            // } else if (window.innerHeight === wrapperRef.current.getBoundingClientRect().top) {
+            //     parallaxMobileRef.current.style.position = 'absolute';
+            //     parallaxMobileRef.current.style.bottom = bottom;
+        } else if (wrapperRef.current.getBoundingClientRect().top < window.innerHeight) {
+            parallaxMobileRef.current.style.position = 'fixed';
+            parallaxMobileRef.current.style.bottom = 0;
+        }
+        // parallaxMobileRef.current.style.transform = `translateY(${window.innerHeight - wrapperRef.current.getBoundingClientRect().top}px)`;
+    };
+    useEffect(() => {
+        setBottom(wrapperRef.current.getBoundingClientRect().height);
+        window.addEventListener('scroll', onScroll);
+        window.requestAnimationFrame(onScroll);
+    }, []);
     return (
-        <div className={classes.parallax} style={{ backgroundImage: `url(${imgUrl})` }}>
+        <div ref={wrapperRef} className={classes.parallax}>
+            <div
+                ref={parallaxMobileRef}
+                className={classes.ParallaxMobile}
+                style={{ backgroundImage: `url(${imgUrl})`, bottom }}
+            />
             <div className={classes.tint}>
-                <div className={classes.TopAngle} />
-                <div className={classes.BottomAngle} />
+                <div className={classes.TopAngle} style={{ borderLeftWidth: window.innerWidth }} />
+                <div className={classes.BottomAngle} style={{ borderLeftWidth: window.innerWidth }} />
             </div>
         </div>
     );
