bcffcbd967662c2c0f4f821cd40e24129c1bd0e1
Abhinn Ankit <abhinnankit@outlook.com>
2019-05-23 16:51:35 -0400

    safari animation fix

--- src/components/UI/Parallax/Parallax.tsx
+++ src/components/UI/Parallax/Parallax.tsx
@@ -7,21 +7,25 @@ const Parallax = () => {
     const parallaxMobileRef = useRef(null);
     const [bottom, setBottom] = useState(0);
     const onScroll = () => {
-        if (
-            window.innerHeight - wrapperRef.current.getBoundingClientRect().top >
-            window.innerHeight + wrapperRef.current.getBoundingClientRect().height
-        ) {
-            parallaxMobileRef.current.style.position = 'absolute';
-            parallaxMobileRef.current.style.bottom = bottom;
-            parallaxMobileRef.current.style.top = 'auto';
-        } else if (wrapperRef.current.getBoundingClientRect().top > window.innerHeight) {
-            parallaxMobileRef.current.style.position = 'absolute';
-            parallaxMobileRef.current.style.bottom = bottom;
-            parallaxMobileRef.current.style.top = 'auto';
-        } else if (wrapperRef.current.getBoundingClientRect().top < window.innerHeight) {
-            parallaxMobileRef.current.style.position = 'fixed';
-            parallaxMobileRef.current.style.bottom = 0;
-            parallaxMobileRef.current.style.top = 0;
+        if (window.matchMedia('only screen and (max-width: 767px)').matches) {
+            if (
+                window.innerHeight - wrapperRef.current.getBoundingClientRect().top >
+                window.innerHeight + wrapperRef.current.getBoundingClientRect().height
+            ) {
+                parallaxMobileRef.current.style.position = 'absolute';
+                parallaxMobileRef.current.style.bottom = bottom;
+                parallaxMobileRef.current.style.top = 'auto';
+            } else if (wrapperRef.current.getBoundingClientRect().top > window.innerHeight) {
+                parallaxMobileRef.current.style.position = 'absolute';
+                parallaxMobileRef.current.style.bottom = bottom;
+                parallaxMobileRef.current.style.top = 'auto';
+            } else if (wrapperRef.current.getBoundingClientRect().top < window.innerHeight) {
+                parallaxMobileRef.current.style.position = 'fixed';
+                parallaxMobileRef.current.style.bottom = 0;
+                parallaxMobileRef.current.style.top = 0;
+            }
+        } else {
+            parallaxMobileRef.current.style.backgroundAttachment = 'fixed';
         }
     };
     useEffect(() => {
@@ -35,7 +39,7 @@ const Parallax = () => {
             <div
                 ref={parallaxMobileRef}
                 className={classes.ParallaxMobile}
-                style={{ backgroundImage: `url(${imgUrl})`, bottom }}
+                style={{ backgroundImage: `url(${imgUrl})` }}
             />
             <div className={classes.tint}>
                 <div className={classes.TopAngle} style={{ borderLeftWidth: window.innerWidth }} />
