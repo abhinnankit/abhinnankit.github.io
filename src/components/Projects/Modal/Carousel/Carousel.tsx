9a5c2a26b274360234e168bbfc9c4055836e65b0
Abhinn Ankit <abhinnankit@outlook.com>
2019-05-27 04:10:52 -0400

    Add animation in carousel

--- src/components/Projects/Modal/Carousel/Carousel.tsx
+++ src/components/Projects/Modal/Carousel/Carousel.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 import classes from './Carousel.module.scss';
 import { ReactComponent as LeftArrow } from '../../../../assets/images/svg/left_arrow.svg';
 import { ReactComponent as RightArrow } from '../../../../assets/images/svg/right_arrow.svg';
@@ -8,23 +8,55 @@ interface IProps {
     carImgUrls: string[];
 }
 
-const Carousel: FC<IProps> = React.memo(props => {
+const Carousel: FC<IProps> = props => {
+    const slideWrapperRef = useRef(null);
     const [carImgUrls, setCarImgUrls] = useState(props.carImgUrls);
+    const [moveForward, setMoveForward] = useState(false);
+    const [moveBackward, setMoveBackward] = useState(false);
+    const carouselSlideWrapperClasses = [classes.CarouselSlideWrapper];
     const onRightArrowHandler = () => {
+        setMoveForward(true);
+    };
+    const shiftElementsForwards = () => {
         const tempImgUrls = [...carImgUrls];
         tempImgUrls.unshift(tempImgUrls.pop());
+        slideWrapperRef.current.removeEventListener('webkitAnimationEnd', shiftElementsForwards);
+        slideWrapperRef.current.removeEventListener('animationEnd', shiftElementsForwards);
+        setMoveForward(false);
         setCarImgUrls(tempImgUrls);
     };
     const onLeftArrowHandler = () => {
+        setMoveBackward(true);
+    };
+    const shiftElementBackwards = () => {
         const tempImgUrls = [...carImgUrls];
         tempImgUrls.push(tempImgUrls.shift());
+        slideWrapperRef.current.removeEventListener('webkitAnimationEnd', shiftElementBackwards);
+        slideWrapperRef.current.removeEventListener('animationEnd', shiftElementBackwards);
+        setMoveBackward(false);
         setCarImgUrls(tempImgUrls);
     };
+    useEffect(() => {
+        if (moveForward) {
+            slideWrapperRef.current.addEventListener('webkitAnimationEnd', shiftElementsForwards);
+            slideWrapperRef.current.addEventListener('animationEnd', shiftElementsForwards);
+        }
+        if (moveBackward) {
+            slideWrapperRef.current.addEventListener('webkitAnimationEnd', shiftElementBackwards);
+            slideWrapperRef.current.addEventListener('animationEnd', shiftElementBackwards);
+        }
+    });
+    if (moveForward) {
+        carouselSlideWrapperClasses.push(classes.Forwards);
+    }
+    if (moveBackward) {
+        carouselSlideWrapperClasses.push(classes.Backwards);
+    }
     return (
         <div className={classes.Carousel}>
-            <div className={classes.CarouselSlideWrapper}>
-                {carImgUrls.slice().map((carImgUrl, index) => (
-                    <CarouselSlide key={carImgUrl} imagesUrl={carImgUrl} imgIndex={index} />
+            <div ref={slideWrapperRef} className={carouselSlideWrapperClasses.join(' ')}>
+                {carImgUrls.slice().map(carImgUrl => (
+                    <CarouselSlide key={carImgUrl} imagesUrl={carImgUrl} />
                 ))}
             </div>
             <span className={classes.Prev} onClick={onLeftArrowHandler}>
@@ -35,6 +67,6 @@ const Carousel: FC<IProps> = React.memo(props => {
             </span>
         </div>
     );
-});
+};
 
 export default Carousel;
--- src/components/Projects/Modal/Carousel/CarouselSlide/CarouselSlide.tsx
+++ src/components/Projects/Modal/Carousel/CarouselSlide/CarouselSlide.tsx
@@ -3,15 +3,10 @@ import classes from './CarouselSlide.module.scss';
 
 interface IProps {
     imagesUrl: string;
-    imgIndex: number;
 }
 
 const CarouselSlide: FC<IProps> = props => {
-    const imgClasses = [classes.CarouselImage];
-    if (props.imgIndex !== 1) {
-        imgClasses.push(classes.invisible);
-    }
-    return <img className={imgClasses.join(' ')} src={props.imagesUrl} alt={''} />;
+    return <img className={classes.CarouselImage} src={props.imagesUrl} alt={''} />;
 };
 
 export default CarouselSlide;
